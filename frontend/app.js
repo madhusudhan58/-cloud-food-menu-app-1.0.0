8493d987e311889d968d975d299f2829c1c63148
madhusudhan58 <lingalamadhusudhan58@gmail.com>
2026-07-08 11:51:29 +0530

    added new of this project

--- frontend/app.js
+++
@@ -1,7 +0,0 @@
-fetch("/api/restaurants")
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById("restaurants").innerHTML =
-      JSON.stringify(data);
-  })
-  .catch(err => console.log(err));
